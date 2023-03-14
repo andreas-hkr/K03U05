5917ccfb77383fbb2d48eee38d95d1dbef164682
Andreas Nilsson <andreas.nilsson@hkr.se>
2023-03-14 17:40:34 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        // Hur många procent av Kol-14 återstår efter S år?
+        //
+        // Formler: n = n0 * e upphöjt till( -lambda * t)
+        //          lambda = ln2 / T
+        //
+        // n0 = 100  (börjar med 100%)
+        // t = S
+        // T = 5730
+
+
+    }
+}
